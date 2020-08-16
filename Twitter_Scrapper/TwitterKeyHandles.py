0ad708c2d8ac1c12461580764dd4adcc96779470
Boddu Srikar <bs422@snu.edu.in>
2020-08-16 23:56:25 +0530

    Assignment01: Able to connect to Twitter using APIs and extract data from twitter.com

---
+++ Twitter_Scrapper/TwitterKeyHandles.py
@@ -0,0 +1,5 @@
+get_api_key = 'BnB7ELEIRhNfqLVnyyHHHtb3t'
+get_api_secret_key = 'pKUTbac4XualkRFdvFzDe8ZgAzbzYVlhV4SgIGLMMdGcNCUrgh' 
+get_access_token = '4713529994-eJnM1B8axPtQbEJuvJk0o6tYqadITtyziTzZfQX'
+get_access_token_secret = 'illtP2gcDcxz44zq6lcVF6bdRlqifFVfHEZp6n8zCaUez'
+
